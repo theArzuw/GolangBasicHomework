755940c4efc43042805e8b5a3cdfea1f880c0751
Arzu <119745513+arzunuzx1@users.noreply.github.com>
2023-01-15 23:24:22 +0300

    Update odev10.go

--- odev10.go
+++ odev10.go
@@ -6,7 +6,7 @@ func main() {
 	arr := []int{30, 60, 40, 70, 30, 50, 10, 80, 40, 50, 70, 20}
 	freq := make(map[int]int)
 	for _, num := range arr {
-		freq[num] = freq[num] + 1
+		freq [num] = freq[num] + 1
 	}
-	fmt.Println("Frequency of the Array is : ", freq)
+	fmt.Println("Frequency of the array is : ", frequency)
 }
