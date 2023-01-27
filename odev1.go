aab16e8c4ea5961412caf60e87af15aefab221a9
Arzu <119745513+arzunuzx1@users.noreply.github.com>
2023-01-27 22:44:03 +0300

    Update odev1.go

--- odev1.go
+++ odev1.go
@@ -5,12 +5,12 @@ import "fmt"
 func main() {
 
 	var num1, num2 int
-	fmt.Println("Enter the First Number :=  ")
+	fmt.Println("Enter the first number :=  ")
 	fmt.Scanln(&num1)
 
-	fmt.Println("Enter the Second Number :=  ")
+	fmt.Println("Enter the second number :=  ")
 	fmt.Scanln(&num2)
 
-	fmt.Println("The Sum of num1 and num2  :=  ", num1+num2)
+	fmt.Println("The sum of num1 and num2  :=  ", num1+num2)
 
 }
