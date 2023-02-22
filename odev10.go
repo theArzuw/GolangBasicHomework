58b1f6c02473cee2dd47d5d402580551fd108724
Arzu Demir <119745513+arzuusadecee@users.noreply.github.com>
2023-02-23 01:31:48 +0300

    Update odev10.go

--- odev10.go
+++ odev10.go
@@ -1,3 +1,9 @@
+/* Question-10 == Given an array, prepare a code and flowchart that gives the number of elements in the array.
+For example :
+Input : arr[] = {3, 3, 3, 3, 2, 3, 4, 1}
+Output : 3 "3" elements, 1 "2" element, 1 "4" element, 1 "1" element. */
+
+
 package main
 
 import "fmt"
